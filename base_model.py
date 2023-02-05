93d7670fdbe7a1d1ac3e1843a5520016c97b9768
TJonCanon <4923@holbertonstudents.com>
2023-02-04 22:13:44 -0600

    task 4

---
+++ base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Base Model module """
+import uuid
+import datetime
+class BaseModel():
+    """ Base Model Class """
+    def __init__(self):
+        """ Initialize Base Model Object """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+        print(self.created_at)
+    def __str__(self):
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        tdn = self.__dict__
+        format_time = {
+            "updated_at": tdn['updated_at'].strftime("%Y-%m-%dT%H:%M:%S.%f"), 
+            "created_at": tdn['created_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
+        }
+        tdn.update(format_time)
+        tdn.update({"__class__": type(self).__name__})
+        return tdn
