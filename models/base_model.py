3b97ed708124e3d45c65583736543824149d8d0e
TJonCanon <4923@holbertonstudents.com>
2023-02-05 17:39:27 -0600

    removed test task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,6 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
-        print(self.created_at)
 
     def __str__(self):
         """ string representation of Base Model object """
