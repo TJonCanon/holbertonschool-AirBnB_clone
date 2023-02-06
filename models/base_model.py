bffa0ba8d9a278bcd9f0c9534be23358d0003cbf
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-05 19:49:00 -0600

    #5 kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,19 @@ import uuid
 import datetime
 class BaseModel():
     """ Base Model Class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialize Base Model Object """
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(value, fmt))
+                    else:
+                        setattr(self, key, value)
+        
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
-        print(self.created_at)
 
     def __str__(self):
         return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
