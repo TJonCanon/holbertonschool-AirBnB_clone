28fce25295f78658f81dab6129004daba7e5877b
TJonCanon <4923@holbertonstudents.com>
2023-02-07 17:58:56 -0600

    unit test for 4, 5, 6, and 9

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" unit testing for file storage """
+import unittest
+from models.engine.file_storage import FileStorage
+import pep8
+
+class TestFileStorage(unittest.TestCase):
+    """ Test File Storage Class """
+
+    def test_pep8(self):
+        """ Test pep8 of files """
+        result = pep8.StyleGuide(quiet=True).check_files(
+            ['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0, "Found Errors + Warnings")
+
+    def test_class_docstring(self):
+        """ Test docstring of Baase Model and its methods """
+        self.assertTrue(len(FileStorage.__doc__) >= 1)
+        self.assertTrue(len(FileStorage.all.__doc__) >= 1)
+        self.assertTrue(len(FileStorage.new.__doc__) >= 1)
+        self.assertTrue(len(FileStorage.save.__doc__) >= 1)
+        self.assertTrue(len(FileStorage.reload.__doc__) >= 1)
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Unit test for user.py file"""
+import inspect
+import unittest
+import pep8
+from models.user import User
+import inspect
+
+class TestBaseModelDocs(unittest.TestCase):
+    """Unittest for BaseModel class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(User, inspect.isfunction)
+
+    def test_pep8_conformance(self):
+        """test for pep8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(
+            ['models/user.py', 'tests/test_models/test_user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstring(self):
+        """Tests for the presence of a module docstring"""
+        self.assertTrue(len(User.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Tests for the presence of a class docstring"""
+        self.assertTrue(len(User.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions"""
+        self.assertTrue(len(User.__init__.__doc__) >= 1)
+        self.assertTrue(len(User.__str__.__doc__) >= 1)
+        self.assertTrue(len(User.save.__doc__) >= 1)
+        self.assertTrue(len(User.to_dict.__doc__) >= 1)
