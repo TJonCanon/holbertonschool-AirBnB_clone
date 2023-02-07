d9cfc325cdbbdbba752f5d355995cbbfd75f869d
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-07 14:26:41 -0600

    added classes

---
+++ models/review.py
@@ -0,0 +1,12 @@
+
+
+#!/usr/bin/python3
+""" Module contains Review subclass """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review class """
+    place_id = ""
+    user_id = ""
+    text = ""
